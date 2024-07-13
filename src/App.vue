891678e962078c77b1b989392c572fd91f7ccbe4
Rai Lombardi <rai.lombardi@atlastechnol.com>
2024-07-13 15:30:24 -0300

    feat: :tada: initial project files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <span> MeuHotel.com </span>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+header {
+  border-bottom: 1px solid $primaryGreen;
+
+  span {
+    font-size: 20px;
+    color: $primaryGreen;
+  }
+}
+</style>
